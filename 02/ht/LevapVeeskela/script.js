813c6a0df21aebdd58dc52949da3ffc3c455310a
DESKTOP-QF7SBGC\Veeskela <paulalexeev10@gmail.com>
2018-04-12 00:34:26 +0300

    *finished work on method of isDeepEqual for HW 02

--- 02/ht/LevapVeeskela/script.js
+++ 02/ht/LevapVeeskela/script.js
@@ -1,19 +1,40 @@
 function isDeepEqual(objA, objB) {
-    // console.log(objA.hasOwnProperty(objB));
-    // console.log(objA in objB);
-
+    var tempArrayProperties = [];
     var stringObject = "object";
     // /* Ваше решение */
     if (isNaN(objA) && isNaN(objB) && String(objA) === "NaN" && String(objA) === "NaN") {
         return true;
-    } else if (Array.isArray(objA) && Array.isArray(objB)) {
+    }
+    if (Array.isArray(objA) && Array.isArray(objB)) {
         return Array(objA).join('') === Array(objB).join('');
     }
-    else if (typeof (objA) === stringObject && typeof (objB) === stringObject) {
+    if (typeof (objA) === stringObject && typeof (objB) === stringObject) {
         return compareObjects(objA, objB);
     }
     else {
         return objA === objB;
+    }
+}
+
+function isDoubleRecurceLinkForObjects(objA, objB) {
+    for (let i in objA) {
+        if (objA[i] === objA) {
+            for (let j in objB) {
+                if (objB[j] === objB) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function getObjectWithoutRecurceLink(object) {
+    for (let i in object) {
+        if (object[i] === object) {
+            delete object[i];
+            return object;
+        }
     }
 }
 
@@ -23,13 +44,17 @@ function compareObjects(objA, objB) {
     var secondArrayKeys = Object.keys(objB);
     var firstArrayValues = Object.values(objA);
     var secondArrayValues = Object.values(objB);
-    for (var i = 0; i < firstArrayKeys.length; i++) {
-        if (secondArrayKeys.some(elem => elem === firstArrayKeys[i])) {
-            resultArray.push(isDeepEqual(firstArrayValues[i], secondArrayValues[secondArrayKeys.indexOf(firstArrayKeys[i])]))
-        } else {
-            resultArray.push(false);
+    if (isDoubleRecurceLinkForObjects(objA, objB)) {
+        return isDeepEqual(getObjectWithoutRecurceLink(objA), getObjectWithoutRecurceLink(objB));
+    } else {
+        for (var i = 0; i < firstArrayKeys.length; i++) {
+            if (secondArrayKeys.some(elem => elem === firstArrayKeys[i])) {
+                resultArray.push(isDeepEqual(firstArrayValues[i], secondArrayValues[secondArrayKeys.indexOf(firstArrayKeys[i])]))
+            } else {
+                resultArray.push(false);
+            }
         }
+        return resultArray.every(elem => elem === true);
     }
-    return resultArray.every(elem => elem === true);
 }
 
